ae8e7d76d26990850dafc82c20366dcdf10ff923
Vitalii <vitalii.skorokhod@gmail.com>
2017-10-26 22:39:59 -0400

    Removed edit post/page link

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -81,22 +81,5 @@ if ( ! function_exists( 'galaxia_entry_footer' ) ) :
 			);
 			echo '</span>';
 		}
-
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'galaxia' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
 	}
 endif;
