eefac28a57f541f273c06cb8b9b1170765adecdf
Vitalii <vitalii.skorokhod@gmail.com>
2017-10-30 00:36:40 -0400

    Removed customizer.php file

--- functions.php
+++ functions.php
@@ -131,8 +131,3 @@ require get_template_directory() . '/inc/custom-header.php';
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
