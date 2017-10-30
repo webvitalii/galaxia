22e0b38c8fcdc20cae41578b4f2eded4393606a0
Vitalii <vitalii.skorokhod@gmail.com>
2017-10-30 00:46:12 -0400

    Updated footer.php
    Updated todo.txt

--- footer.php
+++ footer.php
@@ -1,17 +1,11 @@
 
 
-
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'galaxia' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'galaxia' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'galaxia' ), 'galaxia', '<a href="http://codecraft.universio.net/">webvitaly</a>' );
-			?>
+			<?php _e( 'Powered by', 'galaxia' ); ?>
+			<a href="http://wordpress.org/" title="<?php _e( 'WordPress CMS', 'galaxia' ); ?>" target="_blank"><?php _e( 'WordPress', 'galaxia' ); ?></a>
+			<?php _e( '&', 'galaxia' ); ?>
+			<a href="http://codecraft.universio.net/" title="<?php _e( 'Galaxia WordPress theme', 'galaxia' ); ?>" target="_blank"><?php _e( 'Galaxia', 'galaxia' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
